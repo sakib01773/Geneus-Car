d6304062f2b3041041d1b176c37380154e51a1f1
sakib01773 <sakibshohan1@gmail.com>
2022-11-07 01:47:26 +0600

    Font end done almost

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLMTnnSbOFCxcBv4kp9jB-A528m6I5ln0",
+  authDomain: "module-66-67.firebaseapp.com",
+  projectId: "module-66-67",
+  storageBucket: "module-66-67.appspot.com",
+  messagingSenderId: "365919940823",
+  appId: "1:365919940823:web:ba175315ca84895e85c092"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
